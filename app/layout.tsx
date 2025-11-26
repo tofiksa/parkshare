7f5edf96922a0240d83c8d94bd830ad34a3b8dbe
khadah <khadah@arkivverket.no>
2025-11-26 09:32:51 +0100

    Fiks favicon og CSS warnings - fjern icon.png referanse og -webkit-text-size-adjust

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,6 @@ import { Providers } from "./providers";
 export const metadata: Metadata = {
   title: "Parkshare - Utleie av private parkeringsplasser",
   description: "Finn eller leie ut private parkeringsplasser",
-  icons: {
-    icon: '/icon.png',
-    apple: '/icon.png',
-  },
 };
 
 export default function RootLayout({
