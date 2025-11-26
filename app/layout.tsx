3f4bbfaae43ff358da6a7b7d578f342110a9b740
khadah <khadah@arkivverket.no>
2025-11-26 09:28:34 +0100

    Legg til favicon og fiks CSS warnings

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,10 @@ import { Providers } from "./providers";
 export const metadata: Metadata = {
   title: "Parkshare - Utleie av private parkeringsplasser",
   description: "Finn eller leie ut private parkeringsplasser",
+  icons: {
+    icon: '/icon.png',
+    apple: '/icon.png',
+  },
 };
 
 export default function RootLayout({
